9d6741c483cd1a5c6a0431dd8fb90bad1074df16
onehit <pgalarcon2017@gmail.com>
2022-09-01 20:25:38 -0300

    Ajustes no código + opção Dorama adicionada

--- oneFilm1.0.py
+++ oneFilm1.0.py
@@ -5,18 +5,31 @@ from easygui import *
 from bs4 import BeautifulSoup
 import urllib.request
 
-lista_generos = ['https://www.imdb.com/list/ls068082370/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls000551766/?sort=list_order,asc&st_dt=&mode=detail&page=','https://www.imdb.com/list/ls055874673/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls045017156/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls062101934/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls076655138/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls076166467/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls069376839/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls064252275/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls064287418/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls027738538/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls032616214/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls066247137/?sort=list_order,asc&st_dt=&mode=detail&page=', 'https://www.imdb.com/list/ls008594849/?sort=list_order,asc&st_dt=&mode=detail&page=', 'Sair']
-
-
+lista_generos = ['https://www.imdb.com/list/ls068082370/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls000551766/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls055874673/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls045017156/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls062101934/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls076655138/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls076166467/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls069376839/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls064252275/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls064287418/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls027738538/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls032616214/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls066247137/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls008594849/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'https://www.imdb.com/list/ls094594132/?sort=list_order,asc&st_dt=&mode=detail&page=',
+                 'Sair']
 
 sair = False
 while not sair:
     message = "Selecione um gênero de filme"
     title = "oneFilm1.0"
     choices = ['Todos', 'Comédia', 'Sci-fi', 'Horror', 'Romance', 'Ação', 'Suspense', 'Drama', 'Mistério', 'Crime',
-               'Animação', 'Aventura', 'Fantasia', 'Comédia Romântica', '-------------------------------------------------Sair---------------------------------------------------------']
+               'Animação', 'Aventura', 'Fantasia', 'Comédia Romântica', 'Dorama', '-------------------------------------------------Sair---------------------------------------------------------']
     output = choicebox(message, title, choices)
-    if output == 'Sair':
+    if output == '-------------------------------------------------Sair---------------------------------------------------------':
         exit()
     x = 0
     for i in choices:
@@ -24,7 +37,6 @@ while not sair:
             link_lista = lista_generos[x]
         else:
             x = x + 1
-
 
     def detalhes_filme(detalhe):
         titulo = 'Título:', detalhe['Title']
@@ -39,13 +51,13 @@ while not sair:
         msgbox(msg=str(titulo)+ '\n' +str(genero)+ '\n' +str(ano)+ '\n' +str(diretor)+ '\n' +str(atores)+ '\n' +str(nota)+ '\n' +str(premios)+ '\n' ,title= 'Resultado da Busca', image= 'poster.jpg')
 
 
-    def requisicao(filme):
+    def requisicao_omdb(filme):
         req = requests.get('http://www.omdbapi.com/?i=' + filme + '&apikey=369a3949')
         dicionario = dict(json.loads(req.text))
         return dicionario
 
 
-    def imdb_top_1000_rating():
+    def procurar_id_filmes():
         lista_completa = []
         for i in range(1, 3):
             try:
@@ -59,14 +71,15 @@ while not sair:
                     except:
                         pass
             except:
-                "print('Quantidade de filmes da lista:', len(lista_completa))"
                 break
         return lista_completa
 
 
-    def roleta_russa(imdb_top_1000_rating):
+    def randomizar_filme(imdb_top_1000_rating):
         filme_escolhido = random.choice(imdb_top_1000_rating())
         return filme_escolhido
 
+    def start():
+        detalhes_filme(requisicao_omdb(randomizar_filme(procurar_id_filmes)))
 
-    detalhes_filme(requisicao(roleta_russa(imdb_top_1000_rating)))
+    start()
